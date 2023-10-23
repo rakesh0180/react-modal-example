475f08dbe5f334ac8191334768b9c689b116006a
Rakesh M <rakeshraki0180@gmail.com>
2023-10-23 18:19:35 +0530

    feat: add required packages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap CSS
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
